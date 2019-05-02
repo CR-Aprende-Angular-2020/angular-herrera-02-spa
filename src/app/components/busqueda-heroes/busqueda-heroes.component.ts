84bd8d5e07bb5fdcb9832c810b594412e7088ab9
ChristianRoig <membriyin@gmail.com>
2019-05-02 17:01:29 -0300

    tarea 61a - busqueda works

---
+++ src/app/components/busqueda-heroes/busqueda-heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HeroesService, Heroe } from 'src/app/servicios/heroes.service';
+
+@Component({
+  selector: 'app-busqueda-heroes',
+  templateUrl: './busqueda-heroes.component.html'
+})
+export class BusquedaHeroesComponent implements OnInit {
+
+  heroesB: Heroe[];
+
+  constructor( ){}
+
+  //       private activatedRoute: ActivatedRoute, 
+  //       private _heroesService: HeroesService ) { 
+  //   this.activatedRoute.params.subscribe( params => {
+  //   console.log( params['termino'] );
+    
+  //   this.heroesB = this._heroesService.buscarHeroe( params['termino'] );
+  //   console.log( this.heroesB );
+  //   })
+  // }
+
+  ngOnInit() {
+  }
+
+}
