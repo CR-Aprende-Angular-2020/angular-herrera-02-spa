0acfa29ef4201afbf6140df44250e677881a4dca
ChristianRoig <membriyin@gmail.com>
2019-05-04 13:41:50 -0300

    componente heroeTarjeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BusquedaHeroesComponent } from './components/busqueda-heroes/busqueda-heroes.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { BusquedaHeroesComponent } from './components/busqueda-heroes/busqueda-h
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    BusquedaHeroesComponent
+    BusquedaHeroesComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
